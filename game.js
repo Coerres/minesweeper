d9249c06e4c14280b807d4874abd79aaddc11c4b
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-01-18 10:36:54 +0100

    Add Event-Listener

---
+++ game.js
@@ -0,0 +1,18 @@
+//Event listener
+document.addEventListener('DOMContentLoaded', () => {
+    //Variables declaration
+    const grid = document.querySelector('.grid');
+    const emojiBtn = document.querySelector('.emoji-btn');
+
+    const flagsLeft = document.getElementById('flags-left');
+    const result = document.getElementById('result');
+    const timer = document.getElementById('timer');
+
+    let width = 10;
+    let bombAmount = 5;
+    let flags = 0;
+    let squares = [];
+    let count = 0;
+    let intervalRef = null;
+    let isGameOver = false;
+} )
