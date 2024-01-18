15d3bbdd38dd2c6290e84a7b1afd67551849490e
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-01-18 11:28:45 +0100

    Update function 'createBoard()'

--- game.js
+++ game.js
@@ -16,6 +16,44 @@ document.addEventListener('DOMContentLoaded', () => {
     let intervalRef = null;
     let isGameOver = false;
 
+    //Create game board
+    function createBoard(){
+        //Get shuffled game array with random bombs
+        const bombsArray = Array(bombAmount).fill('bomb');
+        const emptyArray = Array (width * width - bombAmount).fill ('valid');
+        const gameArray = emptyArray.concat(bombsArray);
+        const shuffledArray = gameArray.sort (() => Math.random() - 0.5);
+        emojiBtn.innerHTML = '😀';
+        flagsLeft.innerHTML = bombAmount;
+
+
+
+
+
+
+    }
+
+    //Add flag with right click
+    function addFlag(someInput){
+
+    }
+
+    //Action after clicking on square
+    function click(){
+
+    }
+
+    //Check neighbring squares once square is clicked
+    function checkSquare(){
+
+    }
+
+    //Timer
+    let startTime = function (){}
+
+    //Game over function
+    function gameOver(someInput){}
+})
 
 
 
@@ -25,30 +63,3 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-} )
-
-//Create game board
-function createBoard(){
-
-}
-
-//Add flag with right click
-function addFlag(someInput){
-
-}
-
-//Action after clicking on square
-function click(){
-
-}
-
-//Check neighbring squares once square is clicked
-function checkSquare(){
-
-}
-
-//Timer
-let startTime = function (){}
-
-//Game over function
-function gameOver(someInput){}
