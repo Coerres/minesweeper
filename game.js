4559a70274d213569882748cee896c08ceb57f47
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-01-23 17:47:16 +0100

    Update function 'click(square)'

--- game.js
+++ game.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
             square.addEventListener('click', function (e) {
                 if (isGameOver) { return; }
                 emojiBtn.innerHTML = '😬';
-                click(square);
+                Click(square);
             });
 
             // Left click [for flag checking]
@@ -95,11 +95,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Action after clicking on square
     function click(square) {
-        // Implement the functionality after clicking on a square
-        if (square.classList.contains('bomb')) {
-            gameOver(square);
-        } else {
-            checkSquare(square);
+        let currentId = square.id;
+        if(isGameOver) {return}
+        if(square.classList.contains('checked') || square.classList.contains('flag')) {return};
+        if(square.classList.contains('bomb')){
+
+        }else{
+            let total = square.getAttribute('data');
+            if(total != 0){
+                square.classList.add('checked');
+                if(total == 1) { square.classList.add('one')};
+                if(total == 2) { square.classList.add('two')};
+                if(total == 3) { square.classList.add('three')};
+                if(total == 4) { square.classList.add('four')};
+            }
         }
     }
 
